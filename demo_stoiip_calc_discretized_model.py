b7376a2f3b2422b372b33ebf0837d4a29b6706be
Wisdom-gif <86080394+Wisdom-gif@users.noreply.github.com>
2021-07-15 18:21:00 +0100

    18CN024498 NWACHUKWU TRIUMPH ASSIGNMENT 3
    NWACHUKWU TRIUMPH
    ASSIGNMENT 3

--- demo_stoiip_calc_discretized_model.py
+++ demo_stoiip_calc_discretized_model.py
@@ -8,7 +8,7 @@ nx = int(input('How many blocks there are in x-axis?'))
 ny = int(input('How many blocks there are in y-axis?'))
 boi = float(input('What is the value of initial oil FVF?'))
            
-# discretizing the reservoir
+# discretizing the reservoir to get the reservoirs into blocks(having same properties)
 
 delta_x = Lx/nx
 delta_y = Ly/ny
@@ -18,12 +18,13 @@ area = delta_x*delta_y
 
 # initializing output variable
 
-total_stoiip = 0
+total_stoiip = 0 # 0 is used as a dummy value
  
-# the 'for' loop
+# the 'for' loop note i is usued for clounm and j is for rows, also note the block is named colunm,row e.g 2 column,3row
+# in a 3D way i,j,k, note k indicates layering (all b4 is known as engineering ordering)
 
-for j in range(1,ny+1):
-    for i in range(1,nx+1):
+for j in range(1,ny+1):  # jump from rows to rows (jumping loop occurs after looping has occur through all the colunms in a row) THIS IS THE OUTER LOOP
+    for i in range(1,nx+1): # moves from columns to cloumns (moving loop) THIS IS THE INNER LOOP
         poro = float(input('What is the value of porosity for this block?'))
         sw = float(input('What is the value of water saturation for this block?'))
         block_stoiip = (7758*area*h*poro*(1-sw))/boi
