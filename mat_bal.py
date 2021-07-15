11d6b809410915c23a798d806a43eb521cd0e85c
AbelDawha <86083091+AbelDawha@users.noreply.github.com>
2021-07-15 22:04:10 +0100

    Update mat_bal.py

--- mat_bal.py
+++ mat_bal.py
@@ -1,41 +1,46 @@
-Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-=========== RESTART: C:\Users\ASUS TP500L\OneDrive\Desktop\mat_bal.py ==========
-What is the length of the reservoir in x-direction?:3500
-What is the length of the reservoir in y-direction?:4000
-What is the thickness of the reservoir?300
-How many blocks there are in x-axis?:2
-How many blocks there are in y-axis?:2
-Enter the value of the inital reservoir pressure?:5000
-Enter the value of the effective compressiblity factor?:0.9
-Enter the value of oil compressiblity?:0.8
-Enter the value of the inital oil FVF?:0.6
-Enter the value of the oil FVF at bubblepoint condition?:0.43
-Enter the bubble point pressure value?:2500
-What is the value of porosity for Block 1?:0.6
-What is the value of water saturation for Block 1?:0.45
-What is the current reservioir pressure of the reservoir?:4500
-What is the current value of the oil formation for Block 1?:0.6
-The amount of cummulative oil produced in Block 1 is -1759335267681.84 STB
-The current value of the oil formation factor is -687.5699999999999
-What is the value of porosity for Block 2?:0.56
-What is the value of water saturation for Block 2?:0.48
-What is the current reservioir pressure of the reservoir?:4650
-What is the current value of the oil formation for Block 2?:0.67
-The amount of cummulative oil produced in Block 2 is -1010873162060.15 STB
-The current value of the oil formation factor is -739.17
-What is the value of porosity for Block 3?:0.52
-What is the value of water saturation for Block 3?:0.5
-What is the current reservioir pressure of the reservoir?:4700
-What is the current value of the oil formation for Block 3?:0.72
-The amount of cummulative oil produced in Block 3 is -756034982878.75 STB
-The current value of the oil formation factor is -756.37
-What is the value of porosity for Block 4?:0.48
-What is the value of water saturation for Block 4?:0.58
-What is the current reservioir pressure of the reservoir?:4820
-What is the current value of the oil formation for Block 4?:0.79
-The amount of cummulative oil produced in Block 4 is -333527959982.45 STB
-The current value of the oil formation factor is -797.65
-The total cummulative oil produced from the reservoir is -3859771372603.18 STB
->>> 
+#...TTOWG!
+
+# input statements
+Lx = float(input('What is the length of the reservoir in x-direction?:'))
+Ly = float(input('What is the length of the reservoir in y-direction?:'))
+h = float(input('What is the thickness of the reservoir?'))
+nx = int(input('How many blocks there are in x-axis?:'))
+ny = int(input('How many blocks there are in y-axis?:'))
+pi = float(input("Enter the value of the inital reservoir pressure?:"))
+ce=float(input("Enter the value of the effective compressiblity factor?:"))
+co = float(input("Enter the value of oil compressiblity?:"))
+boi= float(input("Enter the value of the inital oil FVF?:"))
+bob= float(input("Enter the value of the oil FVF at bubblepoint condition?:"))
+pb = float(input("Enter the bubble point pressure value?:"))           
+# discretizing the reservoir
+
+delta_x = Lx/nx
+delta_y = Ly/ny
+
+# calculating the area per block
+area = delta_x*delta_y
+
+# initializing output variable
+
+tnp= 0
+ 
+# the 'for' loop
+
+for j in range(1,ny+1):
+    for i in range(1,nx+1):
+        block_position = (nx*(j-1))+i
+        poro = float(input('What is the value of porosity for Block {0}?:'.format(block_position)))
+        sw = float(input('What is the value of water saturation for Block {0}?:'.format(block_position)))
+        pnow = float(input('What is the current reservioir pressure of the reservoir?:'))
+        bo = float(input('What is the current value of the oil formation for Block {0}?:'.format(block_position)))
+        block_stoiip = (7758*area*h*poro*(1-sw))/boi
+        bo= bob*(1-co*(pnow-pb))
+        np=((block_stoiip*boi*ce)*(pi-pnow))/bo
+        tnp = tnp + np
+        print('The amount of cummulative oil produced in Block {0:.0f} is {1:.2f} STB'.format(block_position, np))
+        print("The current value of the oil formation factor is", bo)
+
+# continuing after the if block
+# displaying the results.
+print('The total cummulative oil produced from the reservoir is {0:.2f} STB'.format(tnp))                       
+
