b516f6f267ab6d7a6d9516ab7ff104fb3d291373
Olatunde Mosobalaje <olatunde.mosobalaje@covenantuniversity.edu.ng>
2021-08-16 17:01:21 +0100

    Updated peteng with stoiip_discretized

--- peteng.py
+++ peteng.py
@@ -90,4 +90,30 @@ def fvf(pressure, temperature, gas_gravity, oil_gravity, pb = None, rs = None, c
 def stoiip(area, thickness, poro, sw, boi):
     N = (7758*area*thickness*poro*(1-sw))/boi
     return round(N, 2)
+
+
+########  A function to compute STOIIP for all blocks in a discretized reservoir ########
+def stoiip_discretized(Lx, Ly, h, nx, ny, boi, poro_list, swi_list):
+    
+    # discretizing the reservoir
+    delta_x = Lx/nx
+    delta_y = Ly/ny
+
+    # calculating the area per block
+    area = delta_x*delta_y
+
+    # initializing output variables
+    total_stoiip = 0
+    stoiip_list =[]
+    
+    # the 'for' loop
+    for j in range(1,ny+1):
+        for i in range(1,nx+1):
+            block_n_order = (nx*(j-1))+i
+            poro = poro_list[(block_n_order - 1)] 
+            sw = swi_list[(block_n_order - 1)]  
+            block_stoiip = (7758*area*h*poro*(1-sw))/boi
+            stoiip_list.append(block_stoiip)  
+            total_stoiip = total_stoiip + block_stoiip
+    return total_stoiip, stoiip_list
     
