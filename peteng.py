024b21308eb5ec30270e5373426a8719bc8402ad
Olatunde Mosobalaje <olatunde.mosobalaje@covenantuniversity.edu.ng>
2021-08-30 14:57:07 +0100

    Updated peteng

--- peteng.py
+++ peteng.py
@@ -91,6 +91,18 @@ def stoiip(area, thickness, poro, sw, boi):
     N = (7758*area*thickness*poro*(1-sw))/boi
     return round(N, 2)
 
+########  A function to compute Stock Tank Oil Initially In-Place (STOIIP), N ########
+# This function accepts a single argument; being a
+# a dictionary
+def stoiip_2(data):
+    N = (7758*data['area']*data['thickness']*data['poro']*(1-data['swi']))/data['boi']
+    return round(N, 2)
+
+# Sample call
+# Uncomment the following lines to run them
+#data_dict = {'area': 40, 'thickness': 15, 'poro': 0.28, 'swi': 0.3, 'boi': 1.2}
+#print(stoiip_2(data_dict))
+
 
 ########  A function to compute STOIIP for all blocks in a discretized reservoir ########
 def stoiip_discretized(Lx, Ly, h, nx, ny, boi, poro_list, swi_list):
