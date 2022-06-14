f46fbdb124df573e1c5e0c87814542c870a06401
Aichienedeosewe <oseweaichienede@gmail.com>
2022-06-14 23:45:01 +0100

    18CN024466
    AICHIENEDE OSEWE DESTINY
    18CN024466
    
    Co-Authored-By: Olatunde Mosobalaje <olatunde.mosobalaje@covenantuniversity.edu.ng>

--- demo_conditional_statement.py
+++ demo_conditional_statement.py
@@ -10,3 +10,9 @@ if initial_pressure > bubble_pressure:
     print('The reservoir is undersaturated!!!')
 else:
     print('The reservoir is saturated!!!')
+
+initial_pressure= input('70')
+bubble_pressure = input(57)
+
+initial_pressure = float(initial_pressure)
+bubble_pressure = float(bubble_pressure)
